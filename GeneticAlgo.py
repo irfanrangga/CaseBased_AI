d929ceaa2ea9d56478f53b5dba098f8dd0781785
Irfan Rangga <irfanrangga04@gmail.com>
2025-04-18 20:45:06 +0700

    nambah function fitness

--- GeneticAlgo.py
+++ GeneticAlgo.py
@@ -1,27 +1,31 @@
 import math
 import random
 
+def fitness(x1, x2):
+    eqt = math.sin(x1) * math.cos(x2) * math.tan(x1+x2) + (3/4) * math.exp(1-(math.sqrt(math.pow(x1, 2))))
+    return -1 * eqt
 
-population_size = 50
-panjang_kromosom = 10
-batasan_x1 = (-10, 10)
-batasan_x2 = (-10, 10)
-bitx = panjang_kromosom // 2
-
-
-def inisialisasi_populasi():
-    return [[random.randint(0, 1) for _ in range(panjang_kromosom)]
-             for _ in range(population_size)]
+# population_size = 50
+# panjang_kromosom = 10
+# batasan_x1 = (-10, 10)
+# batasan_x2 = (-10, 10)
+# bitx = panjang_kromosom // 2
 
 
-def decode(krom):
-    biner_x1 = krom[bitx]
-    biner_x2 = krom[bitx]
+# def inisialisasi_populasi():
+#     return [[random.randint(0, 1) for _ in range(panjang_kromosom)]
+#              for _ in range(population_size)]
 
-    desimal_x1 = int(biner_x1, 2)
-    desimal_x2 = int(biner_x2, 2)
 
-    x1 = X1_MIN + (X1_MAX - X1_MIN) * desimal_x1 / (2**bitx - 1)
-    x2 = X2_MIN + (X2_MAX - X2_MIN) * desimal_x2 / (2**bitx - 1)
-    return x1, x2
+# def decode(krom):
+#     biner_x1 = krom[bitx]
+#     biner_x2 = krom[bitx]
 
+#     desimal_x1 = int(biner_x1, 2)
+#     desimal_x2 = int(biner_x2, 2)
+
+#     x1 = X1_MIN + (X1_MAX - X1_MIN) * desimal_x1 / (2**bitx - 1)
+#     x2 = X2_MIN + (X2_MAX - X2_MIN) * desimal_x2 / (2**bitx - 1)
+#     return x1, x2
+
+print(fitness(2, 6))
